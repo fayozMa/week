7bef1ecd13a08b3e6851ff9a86624b94815e3c89
Muhammadfayyozbek Ahmadjonov <usmonovbahriddin108@gmail.com>
2024-03-28 16:40:53 +0500

    complate

---
+++ index.js
@@ -0,0 +1,25 @@
+let day = prompt('1 dan 7 gacha son kiriting:')
+if(day==1){
+    console.log('Dushanba')
+} 
+else if(day==2){
+    console.log('Seshanba')
+} 
+else if(day==3){
+    console.log('Chorshanba')
+} 
+else if(day==4){
+    console.log('Payshanba')
+} 
+else if(day==5){
+    console.log('Juma')
+} 
+else if(day==6){
+    console.log('Shanba')
+} 
+else if(day==7){
+    console.log('Yakshanba');
+}
+else {
+    console.log('1 dan 7 gacha son kiriting:')
+}
